9b856550237ec88e5738de195721da2a3f7a0832
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2026-04-09 00:29:58 +0530

    feat:added requiremnts

--- src/hepler.py
+++ src/hepler.py
@@ -1,1 +1,10 @@
-import request
+import requests
+def fetch_date(api):
+    try:
+        response = requests.get(api)
+        response.raise_for_status()  # Check if the request was successful
+        return response.json()  # Return the JSON data
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
