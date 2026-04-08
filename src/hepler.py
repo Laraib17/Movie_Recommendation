76e49ceb86ebd9008051e473a655b5236c460b25
Laraib Ahmad <112575218+Laraib17@users.noreply.github.com>
2026-04-09 00:24:29 +0530

    feat:added requiremnts

--- src/hepler.py
+++ src/hepler.py
@@ -1,7 +1,1 @@
-def check_sum_even(numbers):
-    for i in numbers:
-        if i % 2 != 0:
-            return False
-    return True
-def sum_of_squares(numbers):
-    return sum(i**2 for i in numbers)
+import request
